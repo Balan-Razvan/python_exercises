f38a3b170ef99d81ca9d0df0b587cd8acb354f27
razvan balan <2473254@cognizant.com>
2026-03-02 15:53:16 +0200

    tema

--- tema1.py
+++ tema1.py
@@ -1,27 +1,52 @@
 import operator
-# a = 6
-# b = 3
-
-# # operatori logici
-# print(a > 1 and b < 3)
-# print(a > 1 or b < 3)
-# print(not (a > 3))
-
-
-
-# # bitwise
-# print(a & b)
-# print(a | b)
-# print(a ^ b)
-# print(~a)
-
-# print(a << 1)
-# print(a >> 1)
-
 a = int(input('first number '))
 b = int(input('second number '))
-# print(a > b)
 
+
+# operatori logici
+print("and", a > 1 and b < 3)
+print("or", a > 1 or b < 3)
+print("not", not(a > 3))
+
+# bitwise
+print('&', a & b) # AND
+print('|', a | b) # OR
+print('^', a ^ b) # XOR
+print(~a)         # NOT
+print(a << 1)     # left shift
+print(a >> 1)     # right shift
+
+# operatori comparatie
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+# operatori de assignment
+a = 5
+a += 1
+a -= 2
+a *= 2
+a /= 2
+a %= 3
+a //= 2
+a **= 2
+a &= 3
+a |= 3
+a ^= 3
+a <<= 3
+a >>= 3
+
+# special operators
+print('a' in 'apple')
+print('a' not in 'apple')
+print ('a' is 'a')
+
+
+
+# calculator
 operations = {
     '+': operator.add,
     '*': operator.mul,
